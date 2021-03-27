aa0869e359c14ada0478563808e3c322daf23ef9
Carlos Ayala <cayala.w@gmail.com>
2021-03-26 19:32:50 -0500

    issue-1: config, gems and new models
    - add slim, simple_form, tailwind
    - set models, controllers

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  validates :username, presence: true, 
+                        uniqueness: { case_sensitive: false }, 
+                        length: { minimum: 3, maximum: 25 }
+  validates :email, presence: true, 
+                    length: { maximum: 105 }, 
+                    uniqueness: { case_sensitive: false }, 
+                    format: { with: VALID_EMAIL_REGEX }
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  has_many :articles
+end
