444e6a785ddaf8c7e9668d86e28e5d4d8d06cdbf
Carlos Ayala <cayala.w@gmail.com>
2021-03-28 20:35:24 -0500

    issue-3:change controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
-    @articles = Article.all
+    # @articles = Article.all
     @articles = Article.page params[:page]
   end
 
